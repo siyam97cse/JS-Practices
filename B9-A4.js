cca5c65c63cbdb27e9dc8c290bdaccdf42d8d38c
siyam97cse <bgsiyam97@gmail.com>
2024-02-08 08:12:23 +0600

    problem1,2 solved

---
+++ B9-A4.js
@@ -0,0 +1,33 @@
+// Problem-1
+// function calculateMoney(numOfTicket){
+//     const ticketPrice = 120;
+//     const earnMoney = (numOfTicket * ticketPrice);
+//     const guardCost = 500;
+//     const numOfStaff = 8;
+//     const staffCost = 50;
+//     const spendMoney = (guardCost + (numOfStaff*staffCost));
+//     const remainningMoney = (earnMoney - spendMoney);
+//     return remainningMoney;
+// }
+// let value =calculateMoney(100);
+// console.log(value);
+
+
+// Problem-2
+// function checkName(str) {
+//     const lastChar = str[str.length - 1];
+//     const badChar = ['a', 'y', 'i', 'e', 'o', 'u', 'w', 'A', 'Y', 'I', 'E', 'O', 'U', 'W'];
+//     if (badChar.includes(lastChar)) {
+//         console.log('Bad word');
+//     } else {
+//         console.log('Good word');
+//     }
+// }
+// checkName("SalmanI"); 
+
+
+
+
+
+
+
